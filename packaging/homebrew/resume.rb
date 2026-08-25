9366da5aaa2d9d365e1f8ee55fedd8bcf1a0dc4e
Pavel Khmelinsky <hmepas@gmail.com>
2026-08-25 21:57:13 +0300

    * 0.1.4 version

--- packaging/homebrew/resume.rb
+++ packaging/homebrew/resume.rb
@@ -1,26 +1,26 @@
 class Resume < Formula
   desc "Cross-agent AI coding session picker"
   homepage "https://github.com/hmepas/resume"
-  version "0.1.3"
+  version "0.1.4"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/hmepas/resume/releases/download/v#{version}/resume_Darwin_arm64.tar.gz"
-      sha256 "b8b4097a8f54374e7f5e4c089d6c97fb9703160740fc140f3268da6f729bbb6f"
+      sha256 "bb7aeb520dfbcf6b0cd20d62fc9b2b2b72e39328eafc38ac012c368cbffcb181"
     else
       url "https://github.com/hmepas/resume/releases/download/v#{version}/resume_Darwin_x86_64.tar.gz"
-      sha256 "008baf9d32cdefbdceae7476110e178fc86279ca45da57b4b5383d3a21cf9d95"
+      sha256 "d3df1d57eb0627a80e4f1aa33da1041c2d919249add347ac535c514306e98b88"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/hmepas/resume/releases/download/v#{version}/resume_Linux_arm64.tar.gz"
-      sha256 "9ff05a5224540ef37cd9defa51882a5ef235dbb92a053eac686df08f5fd9d0f2"
+      sha256 "0a7ebe227c0b4415f90fdee1b950e867a39dab587f090ec9f63498dcbe895d68"
     else
       url "https://github.com/hmepas/resume/releases/download/v#{version}/resume_Linux_x86_64.tar.gz"
-      sha256 "23e6e0910cb4febd390c6a559b84425bf44fc0188fa63532533ea0e36305644e"
+      sha256 "4c40fa75a9cf5277b82400d57d9056878ca2b02552a2cabe33c461ddc6db933f"
     end
   end
 
